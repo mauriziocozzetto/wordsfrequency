7f0744903339fcccb8e5c59a982bb36c654152b8
mauriziocozzetto <maurizio.cozzetto@gmail.com>
2021-11-11 08:50:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ st.header("Frequenza delle parole in un testo")
 
 uploaded_file = st.file_uploader("Seleziona un file txt dal disco:", accept_multiple_files=False, type=["txt"])
 
+# se non ho caricato nulla mi fermo
 if uploaded_file is None:
     st.stop()
 
+# in stringio c'è il file caricato in memoria
 stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
 
 # creazione del dizionario vuoto
@@ -17,26 +19,29 @@ dct_words = dict()
 
 # elaborazione del file
 for line in stringio:
+    # leggo una linea e la bonifico
     line = line.lower().strip()
 
     # se è vuota la salto
     if line == '':
         continue
     
+    # estreggo le parole dalla linea
     words = line.split(' ')
 
+    # per ogni parola creo una voce nel dizionario
     for word in words:
         if word not in dct_words:
             dct_words[word] = 1
         else:
             dct_words[word] += 1
 
-# creazione di un OrderedDict
+# creazione di un OrderedDict, è un nuovo dizionario ordinato in base al valore x[1], dove x è la tupla)
 dct_freq = collections.OrderedDict(sorted(dct_words.items(), key=lambda t: t[1], reverse=True))
 
 number = st.slider('Quante parole vuoi visualizzare?', 3, 30)
 
-# stampa del diagramma a barre
+# stampa del diagramma a barre, thank you Federico Barbieri
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.bar(list(dct_freq.keys())[:number],list(dct_freq.values())[:number])
